38f3900643f0a7fbbc0fa3402792681160b4853c
VinceCaetano <caetanomainente@outlook.com>
2024-08-24 20:13:02 -0300

    updte

--- src/main/java/com/privatechat/config/SecurityConfig.java
+++ src/main/java/com/privatechat/config/SecurityConfig.java
@@ -9,7 +9,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -37,7 +37,7 @@ public class SecurityConfig {
                 )
                 .formLogin(formLogin ->
                         formLogin
-                                .loginPage("/login") // Specify your custom login page
+                                .loginPage("/login") 
                                 .permitAll()
                 )
                 .httpBasic(withDefaults());
